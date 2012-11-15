904bfa22bfd8985256c2d747f8c501e453ff052d
Maciej Hirsz <maciej.hirsz@gmail.com>
2012-11-15 17:01:49 +0100

    compiled assets

--- package/public/assets/js/bootstrap.js
+++ package/public/assets/js/bootstrap.js
@@ -5,12 +5,7 @@ require.config({
   urlArgs: "bust=" + new Date().getTime(),
   priority: ['jquery'],
   paths: {
-    jquery: 'vendor/jquery/jquery-1.8.0.min',
-    text: 'vendor/requirejs/text',
-    hogan: 'vendor/hoganjs/hogan-2.0.0.amd',
-    underscore: 'vendor/backbone/underscore-1.3.3',
-    backbone: 'vendor/backbone/backbone-0.9.2',
-    rippl: 'vendor/rippl/rippl.min'
+    text: 'vendor/requirejs/text'
   },
   shim: {
     backbone: {
